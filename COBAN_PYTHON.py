b6fa3189e83d809c01dce847cacec236962b53bb
NguyenHoangChuong19022005 <chuongnh4717@ut.edu.vn>
2024-12-10 16:02:58 +0700

    Bai dau

---
+++ COBAN_PYTHON.py
@@ -0,0 +1,14 @@
+#Kieu danh sach
+my_list =[1,2,3,"four",[5,6]]
+print(my_list[0])
+print(my_list[3])
+print(my_list[4][1])
+print(my_list[4][0])
+#Kieu tuple
+my_tuple =[ 1,2,3,5.7]
+print(my_tuple[0])
+print(my_tuple[3])
+print(my_tuple[1:4])
+#Kieu tu dien
+my_dict={'1':'Nguyen','2':'Hoang','3':'Chuong'}
+my_dict.get('1','not one')
